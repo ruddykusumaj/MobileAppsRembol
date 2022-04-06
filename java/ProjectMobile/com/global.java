cdbaa80f8c0da9571f046536c6711ea87ed8d4e3
ruddykusumaj <103119594+ruddykusumaj@users.noreply.github.com>
2022-04-06 19:41:22 +0700

    Delete java directory

--- java/ProjectMobile/com/global.java
+++
@@ -1,9 +0,0 @@
-package ProjectMobile.com;
-
-public class global {
-    public static String uname, uemail;
-
-    public static String gidmobil, gidlist, gnamamobil, ghargasewa, gstatusmobil, gkategori;
-
-    public static String cidjenis, cnamamobil, chargamobil, cstatus;
-}
